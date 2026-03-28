c4c18cb29db7685ca961fc248523af0d11bf6a1a
jama050690 <jbm050690@gmail.com>
2026-03-28 17:58:52 +0500

    your message

--- ps/sql_cod.sql
+++ ps/sql_cod.sql
@@ -12,10 +12,13 @@ Alter Table todos ALTER Column name Set Not Null;
 
 ---mOCK DATA 
 Insert Into users (email) Values ('john.doe@example.com'),('jane.smith@example.com'),('sarah@example.com ');
-Insert Into todos (name,user_id)Values ('A1', 1), ('A2', 2), ('A3', 3),('A4',4);
-Insert Into todos (name,user_id)Values ('B1', 1), ('B2', 2), ('B3', 3),('B4',4);
-Insert Into  todos(name,user_id) Values ('C1',111),('C2',112),('C3',113),('C4',114);
+Insert Into todos (name,user_id)Values ('A1', 1), ('A2', 2), ('A3', 3);
+Insert Into todos (name,user_id)Values ('B1', 1), ('B2', 2), ('B3', 3);
+Insert Into  todos(name,user_id) Values ('C1',111),('C2',112),('C3',113);
 
+UPDATE todos
+SET is_completed = true
+WHERE id IN (1, 2, 3, 4);
 Select u.email,(Select count (id) from todos t where() t.user_id=u.id and t.is_completed=false) as count_of_todos from user u where(Select count (id) from todos t where() t.user_id=u.id) count_of_todos >0 order by couunt_of_todos desc;
 
 SELECT
@@ -28,17 +31,18 @@ SELECT
     ) AS bajarilgan_foiz
 FROM todos;
 
--- SELECT
---     u.id AS user_id,
---     u.email,
---     COUNT(t.id) AS jami_vazifa,
---     COUNT(CASE WHEN t.is_completed = true THEN 1 END) AS bajarildi,
---     COUNT(CASE WHEN t.is_completed = false THEN 1 END) AS bajarilmadi,
---     ROUND(
---         COUNT(CASE WHEN t.is_completed = true THEN 1 END) * 100.0 / COUNT(t.id),
---         2
---     ) AS bajarilgan_foiz
--- FROM users u
--- LEFT JOIN todos t ON t.user_id = u.id
--- GROUP BY u.id, u.email
--- ORDER BY bajarilgan_foiz DESC;
+
+SELECT
+    u.id AS user_id,
+    u.email,
+    COUNT(t.id) AS jami_vazifa,
+    COUNT(CASE WHEN t.is_completed = true THEN 1 END) AS bajarildi,
+    COUNT(CASE WHEN t.is_completed = false THEN 1 END) AS bajarilmadi,
+    ROUND(
+        COUNT(CASE WHEN t.is_completed = true THEN 1 END) * 100.0 / COUNT(t.id),
+        2
+    ) AS bajarilgan_foiz
+FROM users u
+LEFT JOIN todos t ON t.user_id = u.id
+GROUP BY u.id, u.email
+ORDER BY bajarilgan_foiz DESC;
